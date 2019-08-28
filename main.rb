505c8a6e844f166e981e435cf392dfe4eef24a73
Kerron King <kerron@Kerrons-MacBook-Pro.local>
2019-08-28 14:14:09 -0400

    Added any, none and count methods

--- main.rb
+++ main.rb
@@ -7,8 +7,6 @@ module Enumerables
     end
     self
   end
-end
-  end
   def my_each_with_index
     i = index
     self[index] = item
@@ -18,18 +16,19 @@ end
     end
     self
   end
-  end
   def my_select(&block)
     result = []
     self.each do |elem|
-      result << block.call(elem) if block.call(elem) == true
+      result << elem if block.call(elem) == true
+      end
     end
     result
   end
-  def my_all?
+  def my_all?(&block)
     counter = 0
     self.each do |elem|
       counter += 1 if block.call(elem) == true
+      end
     end
     if counter == self.size
       return true
@@ -37,14 +36,36 @@ end
       return false
     end
   end
-  def my_any
-
+  def my_any(&block)
+    counter = 0
+    self.each do |elem|
+      counter += 1 if block.call(elem) == true
+      end
+    end
+    if counter.positive?
+      return true
+    else
+      return false
+    end
   end
-  def my_none?
-
+  def my_none?(&block)
+    counter = 0
+    self.each do |elem|
+      counter += 1 if block.call(elem) == false
+    end
+    if counter == self.size
+      return true
+    else
+      return false
+    end
   end
-  def my_count
-
+  def my_count(&block)
+    counter = 0
+    self.each do |elem|
+      counter += 1 if block.call(elem) == true
+      end
+    end
+    return counter
   end
   def my_map(&block)
     result = []
