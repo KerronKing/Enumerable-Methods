d3aa09b8ffeb75495f6b8b06dd49bc479e1e8fcf
Kerron King <kerron@Kerrons-MacBook-Pro.local>
2019-08-28 17:46:33 -0400

    Methods written. Refinement needed

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-module Enumerables
+module Enumerable
   def my_each
     i = 0
     while i < self.size
@@ -18,17 +18,15 @@ module Enumerables
   end
   def my_select(&block)
     result = []
-    self.each do |elem|
+    self.my_each do |elem|
       result << elem if block.call(elem) == true
-      end
     end
     result
   end
   def my_all?(&block)
     counter = 0
-    self.each do |elem|
+    self.my_each do |elem|
       counter += 1 if block.call(elem) == true
-      end
     end
     if counter == self.size
       return true
@@ -38,9 +36,8 @@ module Enumerables
   end
   def my_any(&block)
     counter = 0
-    self.each do |elem|
+    self.my_each do |elem|
       counter += 1 if block.call(elem) == true
-      end
     end
     if counter.positive?
       return true
@@ -50,7 +47,7 @@ module Enumerables
   end
   def my_none?(&block)
     counter = 0
-    self.each do |elem|
+    self.my_each do |elem|
       counter += 1 if block.call(elem) == false
     end
     if counter == self.size
@@ -59,25 +56,34 @@ module Enumerables
       return false
     end
   end
-  def my_count(&block)
+  def my_count
     counter = 0
-    self.each do |elem|
-      counter += 1 if block.call(elem) == true
+    if block_given?
+      self.my_each do |elem|
+        counter += 1 if yield(elem) == true
       end
+    else
+      counter = self.size
     end
-    return counter
+    counter
   end
   def my_map(&block)
     result = []
-    self.each do |elem|
+    self.my_each do |elem|
       result << block.call(elem)
     end
     result
   end
-  def my_inject
-
+  def my_inject(acc, &block)
+    self.my_each do |elem|
+      acc = block.call(acc, elem)
+    end
+    acc
   end
-  def multiple_els
-
+  def multiple_els(&block)
+    self.my_inject {block.call(elem)}
   end
 end
+
+
+puts [1, 2, 3].my_count 
